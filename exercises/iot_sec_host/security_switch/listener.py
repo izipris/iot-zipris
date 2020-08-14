13b691fcdd83e24d475a0e97ee129a98f40b803f
idzipris <idzipris@ebay.com>
2020-08-14 15:58:12 +0300

    implemented cache-hit of iot packet

--- exercises/iot_sec_host/security_switch/listener.py
+++ exercises/iot_sec_host/security_switch/listener.py
@@ -9,31 +9,48 @@ IFACE = 'eth0'
 CACHE_DNS = load_cache_dns()
 CACHE_MUD = load_cache_mud()
 
-if __name__ == '__main__':
-    pkt = Ether(src="ff:ff:ff:ff:ff:ff", dst="ff:ff:ff:ff:ff:ff") / IP(dst='10.0.5.5', tos=4) / TCP() / 'test'
-    print(pkt.tos)
-    print('Sniffing on %s' % IFACE)
-    sys.stdout.flush()
-    sniff(iface=IFACE, prn=lambda x: handle_pkt(x))
-
-
-def handle_pkt(pkt):
-    print('got a packet')
-    pkt.show2()
-    dscp_value = tos_to_dscp_value(pkt.tos)
-    if dscp_value == 0:
-        action_noniot(pkt)
-    else:
-        action_iot(pkt, dscp_value)
-    sys.stdout.flush()
-
 
 def action_iot(pkt, dscp_value):
-    pass
+    mud_file = get_mud_from_cache(dscp_value)
+    if mud_file is not None:
+        acl_list = mud_file['ietf-access-control-list:acls']['acl']
+        for acl in acl_list:
+            for ace in acl['aces']['ace']:
+                allowed_domain = ace['matches']['ipv4']['ietf-acldns:src-dnsname']
+                ip_addr = get_ip_by_domain(allowed_domain)
+                if pkt[IP].dst == ip_addr:
+                    action_iot_passed(pkt)
+        action_iot_failed(pkt)
 
 
 def action_noniot(pkt):
     pass
+
+
+def action_iot_passed(pkt):
+    pkt[IP].tos = 0
+    try:
+        sendp(pkt, iface=get_if())
+    except KeyboardInterrupt:
+        raise
+
+
+def action_iot_failed(pkt):
+    pass
+
+
+def get_mud_from_cache(dscp_value):
+    for entity in CACHE_MUD['entities']:
+        if entity['dscp_value'] == dscp_value:
+            return load_json_file(entity['mud_file'])
+    return None  # TODO: DSCP value is unknown, unidentified device
+
+
+def get_ip_by_domain(domain):
+    for entity in CACHE_DNS['entities']:
+        if entity['domain'] == domain:
+            return entity['ip']
+    return None  # TODO: cache miss, call DNS
 
 
 def tos_to_dscp_value(tos):
@@ -51,3 +68,22 @@ def get_if():
         print('Cannot find eth0 interface')
         exit(1)
     return captured_iface
+
+
+def handle_pkt(pkt):
+    print('got a packet')
+    pkt.show2()
+    dscp_value = tos_to_dscp_value(pkt[IP].tos)
+    if dscp_value == 0:
+        action_noniot(pkt)
+    else:
+        action_iot(pkt, dscp_value)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    pkt = Ether(src="ff:ff:ff:ff:ff:ff", dst="ff:ff:ff:ff:ff:ff") / IP(dst='10.0.5.5', tos=4) / TCP() / 'test'
+    handle_pkt(pkt)
+    print('Sniffing on %s' % IFACE)
+    sys.stdout.flush()
+    sniff(iface=IFACE, prn=lambda x: handle_pkt(x))
