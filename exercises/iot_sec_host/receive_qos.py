30698fb230a10298321ffcee6990db262c6ca053
idzipris <idzipris@ebay.com>
2020-09-12 21:14:41 +0300

    code refactor and interface update in receive_qos

--- exercises/iot_sec_host/receive_qos.py
+++ exercises/iot_sec_host/receive_qos.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python
 
 import sys
+
 from scapy.all import sniff, get_if_list
 
+# Constants
+ARGS_COUNT = 2
+ARGS_HOST_IDX = 1
+EXIT_CODE_ERROR = 1
+ERROR_USAGE = 'Usage: python receive_qos.py <host name>'
+
+
 def get_if():
-    ifs=get_if_list()
-    iface=None
+    iface = None
     for i in get_if_list():
-        if "eth0" in i:
-            iface=i
-            break;
+        if host_name + '-eth0' in i:
+            iface = i
+            break
     if not iface:
-        print "Cannot find eth0 interface"
-        exit(1)
+        print('Cannot find eth0 interface')
+        exit(EXIT_CODE_ERROR)
     return iface
 
 
 def handle_pkt(pkt):
-    print "got a packet"
+    print('got a packet')
     pkt.show2()
     sys.stdout.flush()
 
 
+def parse_arguments():
+    global host_name
+    if len(sys.argv) != ARGS_COUNT:
+        print(ERROR_USAGE)
+        quit(EXIT_CODE_ERROR)
+    host_name = sys.argv[ARGS_HOST_IDX]
+
+
 def main():
-    iface = 'eth0'
-    print "sniffing on %s" % iface
+    parse_arguments()
+    iface = host_name + '-eth0'
+    print('sniffing on %s' % iface)
     sys.stdout.flush()
     sniff(iface=iface, prn=lambda x: handle_pkt(x))
 
