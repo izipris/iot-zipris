a86d6c1d06b62225bb544dcd9d1d7daf0c966608
idzipris <idzipris@ebay.com>
2020-09-13 00:00:25 +0300

    code refactor of smartnic listener

--- exercises/iot_sec_host/security_switch/listener.py
+++ exercises/iot_sec_host/security_switch/listener.py
@@ -6,7 +6,30 @@ from scapy.all import get_if_list
 from scapy.all import sniff
 from scapy.layers.inet import IP
 
-from exercises.iot_sec_host.security_switch.model.Connection import Connection
+
+class Connection:
+    def __init__(self, ip_src, ip_dst, dscp_value):
+        self.__ip_src = ip_src
+        self.__ip_dst = ip_dst
+        self.__dscp_value = dscp_value
+
+    def get_ip_src(self):
+        return self.__ip_src
+
+    def get_ip_dst(self):
+        return self.__ip_dst
+
+    def get_dscp_value(self):
+        return self.__dscp_value
+
+    def get_tuple(self):
+        return self.__ip_src, self.__ip_dst, self.__dscp_value
+
+
+class WebDriver:
+    def do_post(self, base_url, path, parameters, headers, body):
+        pass  # TODO: implement post request
+
 
 CACHE_FILE_DNS = './security_switch/cache/dns_cache.json'
 CACHE_FILE_MUD = './security_switch/cache/mud_cache.json'
@@ -42,10 +65,13 @@ def parse_arguments():
 
 def is_iot_client(connection):
     if connection.get_dscp_value() == DSCP_NON_IOT:
+        print(str(connection.get_tuple()) + ' NON-IOT')
         return False
     for entity in CACHE_CLIENTS['entities']:
-        if entity['ip'] == connection.get_ip_src() and entity['dscp_value'] == connection.get_dscp_value:
+        if entity['ip'] == connection.get_ip_src() and entity['dscp_value'] == connection.get_dscp_value():
+            print(str(connection.get_tuple()) + ' CLIENT')
             return True
+        print(str(connection.get_tuple()) + ' NON-CLIENT')
     return False
 
 
@@ -61,10 +87,12 @@ def action_iot(connection):
                     ip_addr = get_ip_by_domain(allowed_domain)
                     if connection.get_ip_dst() == ip_addr:
                         connection_verified = True
+                        print(str(connection.get_tuple()) + ' VERIFIED')
                         break
             if connection_verified:
                 break
         if not connection_verified:
+            print(str(connection.get_tuple()) + ' NOT VERIFIED')
             action_block_connection(connection)
     add_to_connections_cache(connection)
 
@@ -74,7 +102,7 @@ def add_to_connections_cache(connection):
 
 
 def action_block_connection(connection):
-    print('SHOULD BE BLOCKED')
+    print('SHOULD BE BLOCKED')  # TODO: send to controller block request
 
 
 def get_mud_from_cache(ip, dscp_value):
@@ -108,15 +136,18 @@ def get_if():
 
 
 def handle_pkt(pkt):
-    print('got a packet')
-    pkt.show2()
-    connection = Connection(pkt[IP].src, pkt[IP].dst, pkt[IP].tos)
-    if is_iot_client(connection) and connection.get_tuple() not in CACHE_CONNECTION:
-        action_iot(connection)
-    sys.stdout.flush()
+    if IP in pkt:
+        print('got a packet')
+        pkt.show2()
+        connection = Connection(pkt[IP].src, pkt[IP].dst, tos_to_dscp_value(pkt[IP].tos))
+        if is_iot_client(connection) and connection.get_tuple() not in CACHE_CONNECTION:
+            print(str(connection.get_tuple()) + ' STARTED HANDLE')
+            action_iot(connection)
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
+    parse_arguments()
     print('Sniffing on %s' % host_name + '-' + IFACE)
     sys.stdout.flush()
     sniff(iface=host_name + '-' + IFACE, prn=lambda x: handle_pkt(x))
