78542df606cb55f875f4e532dc8e84dd48e06565
idzipris <idzipris@ebay.com>
2020-09-25 12:22:47 +0300

    updated the topology

--- exercises/iot_sec_host/infrastructure/iot-zipris-topo.py
+++ exercises/iot_sec_host/infrastructure/iot-zipris-topo.py
@@ -7,31 +7,31 @@ class ZiprisTopo(Topo):
         # Add hosts
         h1 = self.addHost('h1')  # SmartNIC
         h2 = self.addHost('h2')  # IoT Device #1
-        h3 = self.addHost('h3')  # Non-IoT Device
-        h4 = self.addHost('h4')  # IoT Device #2
+        h4 = self.addHost('h4')  # Non-IoT Device
         h5 = self.addHost('h5')  # Valid Destination #1
         h6 = self.addHost('h6')  # Invalid Destination #1
-        h7 = self.addHost('h7')  # Valid Destination #2
-        h8 = self.addHost('h8')  # Invalid Destination #2
+        h7 = self.addHost('h7')  # IoT Device #2
+        h8 = self.addHost('h8')  # Valid Destination #2
+        h9 = self.addHost('h9')  # Invalid Destination #2
         # Add switches
-        s1 = self.addSwitch('s1')  # Home Switch #1
-        s2 = self.addSwitch('s2')  # Home Switch #2
+        s2 = self.addSwitch('s2')  # Home Switch #1
         s3 = self.addSwitch('s3')  # Destination Switch #1
-        s4 = self.addSwitch('s4')  # Destination Switch #2
+        s4 = self.addSwitch('s4')  # Home Switch #2
+        s5 = self.addSwitch('s5')  # Destination Switch #2
         r1 = self.addSwitch('r1')  # Router
         # Add links
-        self.addLink(h2, s1)
-        self.addLink(h3, s1)
+        self.addLink(h2, s2)
         self.addLink(h4, s2)
         self.addLink(h5, s3)
         self.addLink(h6, s3)
         self.addLink(h7, s4)
-        self.addLink(h8, s4)
+        self.addLink(h8, s5)
+        self.addLink(h9, s5)
         self.addLink(h1, r1)
-        self.addLink(s1, r1)
         self.addLink(s2, r1)
         self.addLink(s3, r1)
         self.addLink(s4, r1)
+        self.addLink(s5, r1)
 
 
 topos = {'ziprisTopo': (lambda: ZiprisTopo())}
